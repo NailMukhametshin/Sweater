795cbb7a44eabdb6d27d04a667b4f126ff128879
nail.zubr@gmail.com <Ubn_Ubn1>
2020-10-11 22:44:16 +0300

    added authorization, authentication

--- src/main/java/com/example/sweater/Controller/MainController.java
+++ src/main/java/com/example/sweater/Controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.sweater;
+package com.example.sweater.Controller;
 
 import com.example.sweater.domain.Message;
 import com.example.sweater.repository.MessageRepository;
@@ -7,24 +7,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     MessageRepository messageRepository;
 
-    @GetMapping("/greeting")
+    @GetMapping("/")
     public String greeting(
-            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
-            Map<String, Object> model) {
-        model.put("name", name);
+    ) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(
             Map<String, Object> model
     ) {
@@ -33,7 +29,7 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(
             @RequestParam String text,
             @RequestParam String tag,
@@ -50,11 +46,11 @@ public class GreetingController {
     public String filter(
             @RequestParam String query,
             Map<String, Object> model
-    ){
+    ) {
         Iterable<Message> messages;
         if (!query.isEmpty()) {
             messages = messageRepository.findAllByTag(query);
-        }else{
+        } else {
             messages = messageRepository.findAll();
         }
         model.put("messages", messages);
---
+++ src/main/java/com/example/sweater/Controller/RegistrationController.java
@@ -0,0 +1,39 @@
+package com.example.sweater.Controller;
+
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
+import com.example.sweater.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping ("/registration")
+    public String registration (){
+        return "registration";
+    }
+
+
+    @PostMapping ("/registration")
+    public String addUser(User user, Map<String, Object> model){
+        User byUserName = userRepository.findByUserName(user.getUserName());
+        if (byUserName != null){
+            model.put("message", "User exist!");
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepository.save(user);
+
+        return "redirect:/login";
+    }
+}
